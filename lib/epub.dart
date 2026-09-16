e854ac8d4f7e0cc60ee18b1e5dcd2b1f955d9929
Kaique Farias <kaiquesouzafarias@outlook.com>
2026-09-16 13:42:47 -0300

    refactor: update elivre, epub and txt public APIs

--- lib/epub.dart
+++ lib/epub.dart
@@ -6,6 +6,7 @@ export 'src/features/epub/epub_document.dart';
 export 'src/features/epub/exceptions/exceptions.dart';
 export 'src/features/epub/media_overlays/media_overlay.dart';
 export 'src/features/epub/media_overlays/parse_media_overlay.dart';
+export 'src/features/epub/media_overlays/parse_smil_clock.dart';
 export 'src/features/epub/metadata/epub_metadata_writer.dart';
 export 'src/features/epub/parse_epub_book.dart';
 export 'src/foundation/entities/entities.dart';
--- lib/txt.dart
+++ lib/txt.dart
@@ -1,7 +1,6 @@
 /// TXT and TXTZ parsing entry points.
 library;
 
-export 'src/features/txt/archive/txtz_archive.dart';
-export 'src/features/txt/parse_txt_book.dart';
-export 'src/features/txt/text/txt_document.dart';
+export 'src/features/txt/parse_txt_book.dart'
+    show parseTxtBook, parseTxtzBook, readTxtMetadata, readTxtzMetadata;
 export 'src/foundation/entities/entities.dart';
